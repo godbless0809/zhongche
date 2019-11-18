51e488a18b3c1dd7e279afe62562f35a02bc3e6c
unknown <unknown@example.com>
2019-11-18 16:11:02 +0800

    ceshi
    test

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World31231231232',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
